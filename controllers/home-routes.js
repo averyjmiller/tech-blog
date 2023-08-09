3bf5d389f5a1e9b9e1f8e18f275722e5d817dc28
Avery Miller <averymiller2060@gmail.com>
2023-08-09 14:29:41 -0400

    implemented GET route for the homepage which displays posts if there are any

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+
+        },
+      ],
+    });
+    const posts = postData.map((post) => 
+      post.get({ plain: true })
+    );
+
+    res.render('homepage', { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
