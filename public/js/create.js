b6e1c6d780e4723b586eff8088984658f5991d2d
Avery Miller <averymiller2060@gmail.com>
2023-08-11 12:09:33 -0400

    added front-end logic for creating a new post

---
+++ public/js/create.js
@@ -0,0 +1,33 @@
+const newPostHandler = async (event) => {
+  event.preventDefault();
+
+  document.querySelector('#newpost').classList.add('hidden');
+  document.querySelector('.posts').classList.add('hidden');
+  document.querySelector('.new-post').classList.remove('hidden');
+}
+
+const createFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#form-title').value.trim();
+  const content = document.querySelector('#form-content').value.trim();
+
+  if(title && content) {
+    console.log(title, content);
+    const response = await fetch('api/blog/create', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if(response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post.');
+    }
+  }
+};
+
+document.querySelector('#newpost').addEventListener('click', newPostHandler);
+
+document.querySelector('.post-form').addEventListener('submit', createFormHandler);
